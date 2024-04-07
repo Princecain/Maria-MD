3a3c6042894ed3f305b3d029ae5e771f2095f376
Princecain <166313086+Princecain@users.noreply.github.com>
2024-04-07 18:01:59 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,14 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['09159176927']
+global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//Dios jnr
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "nigeria"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
-
+global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //Dios jnr
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
 global.packname = 'Sticker By'
